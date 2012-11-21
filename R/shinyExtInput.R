957afc75583def8ad252e3f3e288a85147b8d785
Marcio Nicolau <marcionicolau@me.com>
2012-11-21 11:11:48 -0200

    Add InputBinding for password type

---
+++ R/shinyExtInput.R
@@ -0,0 +1,23 @@
+#' Create a password input control
+#' 
+#' Create an input control for entry of password values
+#' 
+#' @param inputId Input variable to assign the control's value to
+#' @param label Display label for the control
+#' @return A password input control that can be added to a UI definition.
+#' 
+#' @examples
+#' passwordInput("passwd", "Password:")
+#' @export
+passwordInput <- function(inputId, label) {
+  addResourcePath(
+    prefix='passwordInput',
+    directoryPath =  system.file('inputExt',
+                                 package='shinyExt')
+    )
+  tagList(
+    singleton(tags$head(tags$script(src = 'inputExt/inputExt.js'))),
+    tags$label(label),
+    tags$input(id = inputId, type="password", value="")
+  )
+}
---
+++ inst/inputExt/inputExt.js
@@ -0,0 +1,34 @@
+// Password input
+var passwordInputBinding = new InputBinding();
+$.extend(passwordInputBinding, {
+  find: function(scope) {
+    return $(scope).find('input[type="password"]');
+  },
+  getId: function(el) {
+    return InputBinding.prototype.getId.call(this, el) || el.name;
+  },
+  getValue: function(el) {
+    return el.value;
+  },
+  setValue: function(el, value) {
+    el.value = value;
+  },
+  subscribe: function(el, callback) {
+    $(el).on('keyup.passwordInputBinding input.passwordInputBinding', function(event) {
+      callback(true);
+    });
+    $(el).on('change.passwordInputBinding', function(event) {
+      callback(false);
+    });
+  },
+  unsubscribe: function(el) {
+    $(el).off('.passwordInputBinding');
+  },
+  getRatePolicy: function() {
+    return {
+      policy: 'debounce',
+      delay: 250
+    };
+  }
+});
+inputBindings.register(passwordInputBinding, 'shiny.passwordInput');
